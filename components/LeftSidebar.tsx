f521fe35e002449605654e4ac8cce80293ad4647
kyle <kaplaugher94@gmail.com>
2024-06-05 16:21:43 -0400

    nav links

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,3 +1,4 @@
+import { sidebarLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -6,9 +7,33 @@ const LeftSidebar = () => {
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6">
-        <Link href="/">
-          <Image src="/logo.png" alt="logo" width={100} height={100} />
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+            NoteCast
+          </h1>
         </Link>
+
+        {sidebarLinks.map((item) => {
+          return (
+            <Link key={item.route} href={item.route}>
+              <p className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  width={23}
+                  height={27}
+                />
+                <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+                  {item.label}
+                </h1>
+              </p>
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
