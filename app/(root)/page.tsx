e7a8321940f43e0cfad3299ac5c311a532db5cef
kyle <kaplaugher94@gmail.com>
2024-06-06 12:50:28 -0400

    set up convex

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
+"use client";
 import NotecastCard from "@/components/NotecastCard";
 import { noteCastData } from "@/constants";
 import React from "react";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const Page = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
@@ -19,6 +23,7 @@ const Page = () => {
             />
           ))}
         </div>
+        {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
       </section>
     </div>
   );
