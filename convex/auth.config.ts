c776e886d3507d42736ce12ae24398e7fc14b613
kyle <kaplaugher94@gmail.com>
2024-06-06 13:24:38 -0400

    clerk/convex auth

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://trusty-cod-94.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
